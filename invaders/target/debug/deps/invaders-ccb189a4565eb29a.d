b7afe688fab540dad02cff5645deb502bc33199e
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-06-28 19:32:51 -0300

    Criando o arquivo players.rs. Modificações no main: inserindo oplayer no game loop; colocando as fn right e left no game loop; adicionando o player no draw & render

--- invaders/target/debug/deps/invaders-ccb189a4565eb29a.d
+++ invaders/target/debug/deps/invaders-ccb189a4565eb29a.d
@@ -1,7 +1,8 @@
-c:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\invaders-ccb189a4565eb29a.rmeta: src\lib.rs src\frame.rs src\render.rs
+c:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\invaders-ccb189a4565eb29a.rmeta: src\lib.rs src\frame.rs src\render.rs src\player.rs
 
-c:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\invaders-ccb189a4565eb29a.d: src\lib.rs src\frame.rs src\render.rs
+c:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\invaders-ccb189a4565eb29a.d: src\lib.rs src\frame.rs src\render.rs src\player.rs
 
 src\lib.rs:
 src\frame.rs:
 src\render.rs:
+src\player.rs:
--- invaders/target/debug/deps/invaders-ce514467774318ef.d
+++ invaders/target/debug/deps/invaders-ce514467774318ef.d
@@ -1,9 +1,10 @@
-C:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\invaders-ce514467774318ef.rmeta: src\lib.rs src\frame.rs src\render.rs
+C:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\invaders-ce514467774318ef.rmeta: src\lib.rs src\frame.rs src\render.rs src\player.rs
 
-C:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\libinvaders-ce514467774318ef.rlib: src\lib.rs src\frame.rs src\render.rs
+C:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\libinvaders-ce514467774318ef.rlib: src\lib.rs src\frame.rs src\render.rs src\player.rs
 
-C:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\invaders-ce514467774318ef.d: src\lib.rs src\frame.rs src\render.rs
+C:\Users\Andrea c\Desktop\ProjetoInvadersRust\invaders\target\debug\deps\invaders-ce514467774318ef.d: src\lib.rs src\frame.rs src\render.rs src\player.rs
 
 src\lib.rs:
 src\frame.rs:
 src\render.rs:
+src\player.rs:
